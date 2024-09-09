324b064b63411b8746ac8e6c651ee49eb2d07e50
MINJUN YU <minjoon0524@gmail.com>
2024-09-09 20:11:24 +0900

    feat:SpringSecurity 활용 MemberDTO설계

---
+++ src/main/java/com/inhatc/empower/dto/MemberDTO.java
@@ -0,0 +1,67 @@
+package com.inhatc.empower.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO extends User {
+    private String eid; // 사번
+    private String name;
+    private String pw;
+    private String department; //부서
+    private String email;
+    private String phone;
+    private String address;
+    private String position;//직급
+    private LocalDate hireDate; //입사일
+    private boolean memberCheck; // 가입 여부
+
+    private List<String> roleNames = new ArrayList<>();
+
+
+    public MemberDTO(String eid, String name, String pw, String department,
+                     String email, String phone, String address, String position,
+                     LocalDate hireDate, boolean memberCheck, List<String> roleNames) {
+        super(eid,pw, roleNames.stream().map(str->
+                new SimpleGrantedAuthority("ROLE_"+str)).collect(Collectors.toList()));
+        this.eid = eid;
+        this.pw = pw;
+        this.name = name;
+        this.department = department;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.position = position;
+        this.hireDate = hireDate;
+        this.memberCheck = memberCheck;
+        this.roleNames = roleNames;
+    }
+    public Map<String, Object> getClaims() {
+        Map<String, Object> dataMap=new HashMap<>();
+        dataMap.put("eid", eid);
+        dataMap.put("pw",pw);
+        dataMap.put("name", name);
+        dataMap.put("department", department);
+        dataMap.put("email", email);
+        dataMap.put("phone", phone);
+        dataMap.put("address", address);
+        dataMap.put("position", position);
+        dataMap.put("hireDate", hireDate);
+        dataMap.put("memberCheck", memberCheck);
+        dataMap.put("roleNames", roleNames);
+        return dataMap;
+    }
+
+}
