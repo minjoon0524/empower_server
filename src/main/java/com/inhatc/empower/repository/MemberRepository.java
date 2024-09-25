6f0708216322dba06ce0c5962fb7693f8ac61144
MINJUN YU <minjoon0524@gmail.com>
2024-09-25 10:00:29 +0900

    feat: MemberList 조회를 위한 Query문 작성

--- src/main/java/com/inhatc/empower/repository/MemberRepository.java
+++ src/main/java/com/inhatc/empower/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package com.inhatc.empower.repository;
 
 import com.inhatc.empower.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface MemberRepository extends JpaRepository<Member,String> {
 
@@ -12,5 +17,10 @@ public interface MemberRepository extends JpaRepository<Member,String> {
     @EntityGraph(attributePaths = {"memberRoleList"})
     @Query("select m from Member m where m.eid = :eid")
     Member getWithRoles(@Param("eid") String eid);
+
+    @Query("select m, r from Member m left join m.memberRoleList r")
+    Page<Object[]> getMemberList(Pageable pageable);
+
+
 
 }
