4b4bf1f5ab2a01e2dcec89c82d3365a4fca4423c
MINJUN YU <minjoon0524@gmail.com>
2024-09-25 10:06:25 +0900

    feat: 사용자 조회를 위한 DTO 개발

---
+++ src/main/java/com/inhatc/empower/dto/MemberSearchDTO.java
@@ -0,0 +1,29 @@
+package com.inhatc.empower.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberSearchDTO {
+    private String eid; // 사번
+    private String name;
+    private String pw;
+    private String department; //부서
+    private String email;
+    private String phone;
+    private String address;
+    private String position;//직급
+    private LocalDate hireDate; //입사일
+    private boolean memberCheck; // 가입 여부
+
+    @Builder.Default
+    private List<String> roleNames = new ArrayList<>();
+}
