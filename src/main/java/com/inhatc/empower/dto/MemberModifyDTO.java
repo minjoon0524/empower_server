560a9afd1eb47a3bb67fd6577a06750e86a4d280
MINJUN YU <minjoon0524@gmail.com>
2024-10-15 21:53:34 +0900

    feat: 회원수정을 위한 필드 추가(입사일,이름)

--- src/main/java/com/inhatc/empower/dto/MemberModifyDTO.java
+++ src/main/java/com/inhatc/empower/dto/MemberModifyDTO.java
@@ -1,14 +1,24 @@
 package com.inhatc.empower.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class MemberModifyDTO {
-    private String eid;
-    private String department;
+    private String eid; // 사번
+    private String name;
     private String pw;
+    private String department; //부서
+    private String email;
     private String phone;
     private String address;
-    private String position;
+    private String position;//직급
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate hireDate; //입사일
+    private boolean memberCheck; // 가입 여부
+
 
 }
