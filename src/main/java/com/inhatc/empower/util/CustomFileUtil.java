3598e3377e8aaeb14b6f0268ee4edbeb775efdd4
MINJUN YU <minjoon0524@gmail.com>
2024-10-22 20:29:15 +0900

    refactor:list에서 하나의 파일을 받고 저장 할 수 있도록 수정

--- src/main/java/com/inhatc/empower/util/CustomFileUtil.java
+++ src/main/java/com/inhatc/empower/util/CustomFileUtil.java
@@ -17,16 +17,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 
 @Component
 @Log4j2
 @RequiredArgsConstructor
-//파일을 업로드하는 애
-//파일을 다운로드 하는 애
 public class CustomFileUtil {
     @Value("${org.zerock.upload.path}")
     private String uploadPath;
@@ -42,55 +38,53 @@ public class CustomFileUtil {
         log.info("-------------------------------------");
         log.info(uploadPath);
     }
-    public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException{
-        if(files == null || files.size() == 0){
+
+    // 프로필 사진을 저장하는 메서드
+    public String saveProfilePicture(MultipartFile file) throws RuntimeException {
+        if (file == null || file.isEmpty()) {
             return null;
         }
-        List<String> uploadNames = new ArrayList<>();
-        for (MultipartFile multipartFile : files) {
-            String savedName = UUID.randomUUID().toString() + "_"
-                    + multipartFile.getOriginalFilename();
-            Path savePath = Paths.get(uploadPath, savedName);
-            try {
-                Files.copy(multipartFile.getInputStream(), savePath); // 원본파일 업로드
 
-                String contentType = multipartFile.getContentType();
-                if(contentType != null && contentType.startsWith("image")){ //이미지여부 확인
-                    Path thumbnailPath = Paths.get(uploadPath, "s_"+savedName);
-                    Thumbnails.of(savePath.toFile())
-                            .size(200,200)
-                            .toFile(thumbnailPath.toFile());
-                }
+        String savedName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        Path savePath = Paths.get(uploadPath, savedName);
+        try {
+            Files.copy(file.getInputStream(), savePath); // 원본 파일 업로드
 
-                uploadNames.add(savedName);
-            } catch (IOException e) {
-                throw new RuntimeException(e.getMessage());
+            String contentType = file.getContentType();
+            if (contentType != null && contentType.startsWith("image")) { // 이미지 여부 확인
+                Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+                Thumbnails.of(savePath.toFile())
+                        .size(200, 200)
+                        .toFile(thumbnailPath.toFile());
             }
-        }//end for
-        return uploadNames;
+
+            return savedName; // 저장된 파일 이름 반환
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     public ResponseEntity<Resource> getFile(String fileName) {
-        Resource resource = new FileSystemResource(uploadPath+File.separator+fileName);
-        if(!resource.exists()) {
-            resource = new FileSystemResource(uploadPath+File.separator+"default.jpeg");
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+        if (!resource.exists()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
         }
         HttpHeaders headers = new HttpHeaders();
-        try{
+        try {
             headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
-        } catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok().headers(headers).body(resource);
     }
 
-    //    서버 내부에서 파일의 삭제
+    // 서버 내부에서 파일의 삭제
     public void deleteFiles(List<String> fileNames) {
-        if(fileNames == null || fileNames.size() == 0){
+        if (fileNames == null || fileNames.size() == 0) {
             return;
         }
-        fileNames.forEach(fileName-> {
-            //썸네일이 있는지 확인하고 삭제
+        fileNames.forEach(fileName -> {
+            // 썸네일이 있는지 확인하고 삭제
             String thumbnailFileName = "s_" + fileName;
             Path thumbnailPath = Paths.get(uploadPath, thumbnailFileName);
             Path filePath = Paths.get(uploadPath, fileName);
@@ -101,9 +95,5 @@ public class CustomFileUtil {
                 throw new RuntimeException(e.getMessage());
             }
         });
-
     }
-
-
 }
-
