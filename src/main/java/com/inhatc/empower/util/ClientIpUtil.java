5778b37b32eefc3de77009aabe646cf04cf2d21b
MINJUN YU <minjoon0524@gmail.com>
2024-11-04 19:25:45 +0900

    feat:클라이언트 접속 IP를 알아내기 위한 Util

---
+++ src/main/java/com/inhatc/empower/util/ClientIpUtil.java
@@ -0,0 +1,28 @@
+package com.inhatc.empower.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ClientIpUtil {
+    public static String getClientIP(HttpServletRequest request) {
+        String[] headers = {"Proxy-Client-IP",
+                "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR",
+                "X-Real-IP", "X-RealIP", "REMOTE_ADDR"};
+        String ip = request.getHeader("X-Forwarded-For");
+
+        for (String header : headers) {
+            if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader(header);
+            }
+        }
+
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        if(ip.equals("0:0:0:0:0:0:0:1")){
+            ip = "127.0.0.1";
+        }
+
+        return ip;
+    }
+}
