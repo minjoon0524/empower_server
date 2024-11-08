e8e3b163c3c716fe1d4d6a1bfc9df4e99fb1802b
MINJUN YU <minjoon0524@gmail.com>
2024-11-08 23:32:57 +0900

    feat:휴가 관련 enum 타입

---
+++ src/main/java/com/inhatc/empower/constant/LeaveType.java
@@ -0,0 +1,12 @@
+package com.inhatc.empower.constant;
+
+
+public enum LeaveType {
+    GENERAL, //일반
+    HALF_DAY, // 반차
+    SICK_LEAVE, // 병가
+    CONDOLENCE_SPOUSE, // 조사(배우자)
+    CONDOLENCE_PARENT, //조사(부모님)
+    CONDOLENCE_SIBLING, // 조사(형제)
+    MILITARY_SERVICE; // 예비군
+}
---
+++ src/main/java/com/inhatc/empower/constant/MemberVacationStatus.java
@@ -0,0 +1,7 @@
+package com.inhatc.empower.constant;
+
+public enum MemberVacationStatus {
+    PENDING, //대기
+    APPROVE,//승인
+    REJECT //거절
+}
