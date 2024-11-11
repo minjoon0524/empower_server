4cbda0e28d277e455a3f7b8c1e962e3085fec017
MINJUN YU <minjoon0524@gmail.com>
2024-11-11 15:21:49 +0900

    feat:가시성을 위해 changeVacStatus 함수 추가

--- src/main/java/com/inhatc/empower/domain/MemberVacation.java
+++ src/main/java/com/inhatc/empower/domain/MemberVacation.java
@@ -28,13 +28,14 @@ public class MemberVacation extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private LeaveType vacType; //일반(반차), 병가 , 조사(배우자5) ,조사(부모님5),조사(형제1), 예비군
     @Enumerated(EnumType.STRING)
-    private MemberVacationStatus vacStatus; //승인상태
+    private MemberVacationStatus vacStatus; //승인상태(관리자)
 
     private LocalDate vacStartDate; // 시작일
     private LocalDate vacEndDate; // 종료일
     private String vacDescription; //사유
 
-
-
-
+    // 관리자 승인
+    public void changeVacStatus(MemberVacationStatus vacStatus) {
+        this.vacStatus = vacStatus;
+    }
 }
