13c704f4540da81afacaf8124740ee1a2c7a4279
MINJUN YU <minjoon0524@gmail.com>
2024-11-11 15:19:38 +0900

    feat:휴가 전체 리스트,회원별 리스트, 승인 기능 추가

--- src/main/java/com/inhatc/empower/controller/MemberVacationController.java
+++ src/main/java/com/inhatc/empower/controller/MemberVacationController.java
@@ -1,6 +1,9 @@
 package com.inhatc.empower.controller;
 
+import com.inhatc.empower.dto.MemberVacationAttendanceDTO;
 import com.inhatc.empower.dto.MemberVacationDTO;
+import com.inhatc.empower.dto.PageRequestDTO;
+import com.inhatc.empower.dto.PageResponseDTO;
 import com.inhatc.empower.service.MemberVacationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -25,12 +28,36 @@ public class MemberVacationController {
         return memberVacationService.detailsVacation(vacId);
     }
 
+    // 휴가 전체 리스트 조회(전회원, 관리자용)
+    @GetMapping("/list")
+    public PageResponseDTO<MemberVacationDTO> getMemberVacationList(PageRequestDTO pageRequestDTO){
+        log.info("============== 휴가 전체 리스트 조회(전회원, 관리자용) ==============");
+        log.info("list..........." + pageRequestDTO);
+        return memberVacationService.getAllVacationList(pageRequestDTO);
+    }
+
+    // 로그인한 사용자 휴가리스트 조회
+    @GetMapping("/member/{eid}")
+    public PageResponseDTO<MemberVacationDTO> getVacation(@PathVariable(name = "eid") String eid,PageRequestDTO pageRequestDTO){
+        log.info("============== 특정 회원 휴가 리스트 조회 ==============");
+        log.info("list..........." + pageRequestDTO);
+        return memberVacationService.getOneVacationList(pageRequestDTO,eid);
+    }
+
     // 휴가등록
     @PostMapping("/register")
     public Map<String,Long> registerVacation(@RequestBody MemberVacationDTO memberVacationDTO){
         log.info("============== 휴가 등록 ==============");
         Long vacId = memberVacationService.insertVacation(memberVacationDTO);
         return Map.of("result",vacId);
+    }
+
+    // 휴가 승인(관리자)
+    @PostMapping("/approve")
+    public Map<String,String> approveVacation(@RequestBody MemberVacationAttendanceDTO memberVacationAttendanceDTO){
+        log.info("============== 휴가(승인 OR 거절) ==============");
+        String result = memberVacationService.approveVacation(memberVacationAttendanceDTO);
+        return Map.of("Approve Result : ",result);
     }
 
     // 휴가수정
@@ -50,4 +77,8 @@ public class MemberVacationController {
         memberVacationService.deleteVacation(vacId);
         return Map.of("Modify Result : ",vacId);
     }
+
+
+
+
 }
