ecfb147ba5eebbb2fe1b0a542933211ebead9c91
MINJUN YU <minjoon0524@gmail.com>
2024-11-12 22:09:22 +0900

    fix:LocalDate 타입일 경우 JsonFormat 사용 불가로 삭제 및 생성자 오류로 어노테이션 추가

--- src/main/java/com/inhatc/empower/dto/MemberModifyDTO.java
+++ src/main/java/com/inhatc/empower/dto/MemberModifyDTO.java
@@ -1,14 +1,17 @@
 package com.inhatc.empower.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class MemberModifyDTO {
     private String eid; // 사번
@@ -19,11 +22,9 @@ public class MemberModifyDTO {
     private String phone;
     private String address;
     private String position;//직급
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate hireDate; //입사일
     private boolean memberCheck; // 가입 여부
     private String profileName; // 프로필 사진 파일 이름 추가
-    @Builder.Default
-    private List<String> roleNames = new ArrayList<>();
+
 
 }
